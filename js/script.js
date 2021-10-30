2c832504d08c1d2a5ddea2d952f46ebdc5a88813
WebProjects5 <93424485+WebProjects5@users.noreply.github.com>
2021-10-30 15:48:05 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let d = new Date();  
+    function setTimer() {
+        d = new Date();  
+        let year =  d.getMonth()>9 ? d.getFullYear()+1 :  d.getFullYear();
+        let d2 = new Date(year,10,16,0,0);
+        var milliseconds = d2-d;
+        var day, hour, minute, seconds;
+        seconds = Math.floor(milliseconds / 1000);
+        minute = Math.floor(seconds / 60);
+        seconds = seconds % 60;
+        hour = Math.floor(minute / 60);
+        minute = minute % 60;
+        day = Math.floor(hour / 24);
+        hour = hour % 24;
+        document.getElementById("header2").innerHTML= "<h3> There are "+day+" days, "+hour+" hours, "+minute+" minutes and "+seconds+" seconds to go for your next birthday</h3>";
+    }
+if(d.getMonth()==10 && d.getDate()==16){
+    document.getElementById("header1").style.display="block";
+    document.getElementById("header2").style.display="none";
+    
+}
+else{
+    document.getElementById("header1").style.display="none";
+    document.getElementById("header2").style.display="block";
+    setInterval(setTimer, 1000);
+}
+setInterval(showTime, 1000);
+function showTime() {
+
+    let d1 = new Date();
+    let d2 = new Date(2001,10,16,5,5,0);
+    let d = d1-d2;
+    let secs= Math.floor(d/1000);
+    let mins = Math.floor(secs/60);
+    let hours = Math.floor(mins/60);
+    let days = Math.floor(hours/24);
+    let months = Math.floor(days/30);
+    let years = Math.floor((days-4)/365);
+
+  
+    let currentTime =  years+ " years, "+months+" months, "+days+" days, " + hours + " hours, " + mins + " minutes, " + secs+" seconds ";
+  
+    document.getElementById("clock")
+            .innerHTML = currentTime;
+}
+
+
+
