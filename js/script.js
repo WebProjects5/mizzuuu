bd3500c4bb742807dc90248ddab8782417bb96b8
WebProjects5 <93424485+WebProjects5@users.noreply.github.com>
2021-11-13 18:10:06 +0530

    final testing for mobile notification

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+navigator.serviceWorker.register('sw.js');
+
 if (!window.Notification) {
   console.log("Browser does not support notifications.");
 } else {
@@ -5,10 +7,8 @@ if (!window.Notification) {
       Notification.requestPermission();
 }
 
-localStorage.setItem("notNotified1",true);
-localStorage.setItem("notNotified2",true);
 
-var bday = false;
+var bday = false, notNotified1,notNotified2;
 let clicked = 0;
 var d = new Date();
   function setTimer() {
@@ -20,7 +20,6 @@ var d = new Date();
       }
 
       else year = d.getFullYear();
-
 
       let d2 = new Date(year,10,16,0,0);
       var milliseconds = d2-d;
@@ -35,39 +34,30 @@ var d = new Date();
       document.getElementById("header2").innerHTML= "<h3> There are "+day+" days, "+hour+" hours, "+minute+" minutes and "+seconds+" seconds to go for your next birthday</h3>";
   }
 
-  
 function showTime() {
   let d1 = new Date();
   let d2 = new Date(2001,10,16,5,5,0);
   
-  if(d1.getMonth()==10 && d1.getDate()==13){
+  if(d1.getMonth()==10 && d1.getDate()==16){
       bday = true;
       document.getElementById("header1").style.display="block";
       document.getElementById("footer1").style.display="block";
       document.getElementById("header2").style.display="none";
-      
   }
   else{
       bday = false;
       document.getElementById("header1").style.display="none";
       document.getElementById("footer1").style.display="none";
-      document.getElementById("header2").style.display="block";   
+      document.getElementById("header2").style.display="block";  
   }
-  if (
-    d1.getMonth() === 10 &&
-    d1.getDate() === 13 && 
-    d1.getHours() < 18
-  ) {
+  // and here 
+  if ( d1.getMonth() === 10 && d1.getDate() === 13 && d1.getHours() < 18 ) {
     notifyUser(1);
   }
-  if (
-    d1.getMonth() === 10 &&
-    ((d1.getDate() === 13 && d1.getHours() === 18) || d1.getDate()==14)
-  ) {
+  if (d1.getMonth() === 10 && d1.getDate() >= 13 && d1.getHours() === 19) {
     notifyUser(2);
   }
-  
-  
+
   let d = d1-d2;
   let secs= Math.floor(d/1000);
   let mins = Math.floor(secs/60); 
@@ -76,29 +66,27 @@ function showTime() {
   let months = Math.floor(days/30);
   let years = Math.floor((days-4)/365);
 
-
   let currentTime =  years+ " years, "+months+" months, "+days+" days, " + hours + " hours, " + mins + " minutes, " + secs+" seconds ";
 
-  document.getElementById("clock")
-          .innerHTML = currentTime;
+  document.getElementById("clock").innerHTML = currentTime;
           
 }
 
 setInterval(showTime, 1000);
 
 if(!bday){
-  
 setInterval(setTimer,1000);
 }
-if(d.getDate()===18){
-  localStorage.setItem("notNotified1",true);
-  localStorage.setItem("notNotified2",true);
-
+// change here
+if(d.getDate()!=13){ 
+  notNotified2 = true;
+  notNotified2 = true; 
 }
 
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
+
 span.onclick = function() {
 modal.style.display = "none";
 }
@@ -106,38 +94,29 @@ modal.style.display = "none";
 window.onclick = function(event) {
 if (event.target == modal) {
   modal.style.display = "none";
-}
-}
+}}
+
 function secretMsg(){
   clicked +=1;
   if(clicked>=5){
       modal.style.display = "block";
       clicked = 0;
   }
-
 }
 
 function notifyUser(i) {
-  let notNotified1 = localStorage.getItem("notNotified1");
-  let notNotified2 = localStorage.getItem("notNotified2");
     if (Notification.permission === "granted" && i==1 && notNotified1) {
-      var notify = new Notification("Happy Birthday Mizzuuuu 🎉🎂", {
-        body: "Happy birthday idiot have a nice day 🥰",
-        vibrate: true
-      });
-      localStorage.setItem("notNotified1",false);
-
+      navigator.serviceWorker.ready.then( function( registration ){
+                registration.showNotification( "Happy Birthday Mizzuuuu!!!", { body:"Happy birthday idiot. Have a nice day and enjoy a lot ok 😉" } );
+            } );
+      notNotified1 = false;
     }
 
     if (Notification.permission === "granted" && i==2 && notNotified2) {
-      var notify = new Notification("Happy Birthday Mizzuuuu 🎉🎂", {
-        body:
-          "Last wish is also mine 😜. How was your day mizzuuu ? Hope u enjoyed it a lot.",
-        vibrate: true
-      });
-      localStorage.setItem("notNotified2",false);
-
-
-    }
+      navigator.serviceWorker.ready.then( function( registration ){
+                registration.showNotification( "Happy Birthday Mizzuuuu 🎉🎂", { body:"Last wish is also mine 😜. How was your day mizzuuu ? Hope u enjoyed a lot." } );
+            } );
+      notNotified2 = false;
+    } 
   
 }
